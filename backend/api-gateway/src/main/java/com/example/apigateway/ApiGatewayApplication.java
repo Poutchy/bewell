1fb03d45ae808c960c607713beda96af06278eb0
Benslimane Taha <taha.benslimane@etu.umontpellier.fr>
2025-07-01 09:11:36 +0200

    Implemented Authentification with JWT

--- backend/api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ backend/api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -2,12 +2,12 @@ package com.example.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
-
 }
---
+++ backend/auth-service/src/main/java/com/example/authservice/controller/AuthController.java
@@ -0,0 +1,77 @@
+package com.example.authservice.controller;
+
+import com.example.authservice.model.User;
+import com.example.authservice.repository.UserRepository;
+import com.example.authservice.security.JwtUtils;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    AuthenticationManager authenticationManager;
+    UserRepository userRepository;
+    PasswordEncoder encoder;
+    JwtUtils jwtUtils;
+
+    public AuthController(AuthenticationManager authenticationManager, UserRepository userRepository, PasswordEncoder encoder, JwtUtils jwtUtils) {
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.encoder = encoder;
+        this.jwtUtils = jwtUtils;
+    }
+
+    @PostMapping("/signin")
+    public String authenticateUser(@RequestBody User user) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        user.getUsername(),
+                        user.getPassword()
+                )
+        );
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        return jwtUtils.generateToken(userDetails.getUsername());
+    }
+
+    @PostMapping("/client-signup")
+    public String registerClient(@RequestBody User user) {
+        if (userRepository.existsByUsername(user.getUsername())) {
+            return "Error: Username is already taken!";
+        }
+        // Create new user's account
+        User newUser = new User(
+                null,
+                user.getUsername(),
+                encoder.encode(user.getPassword()),
+                "client"
+        );
+        userRepository.save(newUser);
+        return "User registered successfully!";
+    }
+
+    @PostMapping("/salon-signup")
+    public String registerSalon(@RequestBody User user) {
+        if (userRepository.existsByUsername(user.getUsername())) {
+            return "Error: Username is already taken!";
+        }
+        // Create new user's account
+        User newUser = new User(
+                null,
+                user.getUsername(),
+                encoder.encode(user.getPassword()),
+                "salon"
+        );
+        userRepository.save(newUser);
+        return "User registered successfully!";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+
+}
