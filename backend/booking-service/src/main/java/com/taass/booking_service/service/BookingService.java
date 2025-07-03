84821e67f9c377d8b07bf61e5dff10c01a7b03a5
Matteo Bussolino <matteobussolino7@gmail.com>
2025-07-04 01:26:35 +0200

    Changed the validation methods, now they save the needed values to be sent to the notification service

--- backend/booking-service/src/main/java/com/taass/booking_service/service/BookingService.java
+++ backend/booking-service/src/main/java/com/taass/booking_service/service/BookingService.java
@@ -17,7 +17,11 @@ import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -27,7 +31,9 @@ public class BookingService {
     private final RabbitTemplate rabbitTemplate;
     private final WebClient clientWebClient;
     private final WebClient salonWebClient;
+
     private BookingStatus bookingStatus;
+    private final HashMap<String, String> bookingDetails = new HashMap<>();
 
     @Value("${app.rabbitmq.exchange}")
     private String exchangeName;
@@ -55,20 +61,38 @@ public class BookingService {
         Booking savedBooking =  bookingRepository.save(booking);
 
         bookingStatus = BookingStatus.PENDING_VALIDATION; // Default status
+
         boolean isBookingValid = validateBookingRequest(bookingRequest);
+
+
+// Uncomment the following lines to simulate a bookingDetails map for testing purposes
+//        bookingDetails.put("salonName", (String) "salonName");
+//        bookingDetails.put("salonAddress", (String) "salonAddress");
+//        bookingDetails.put("salonPhone", (String) "3493618984");
+//        bookingDetails.put("salonEmail", (String) "salonEmail@emal.com");
+//        bookingDetails.put("clientName", (String) "Matteo");
+//        bookingDetails.put("clientSurname", (String) "Bussolino");
+//        bookingDetails.put("clientEmail", (String) "matteobussolino7@gmail.com");
+//        bookingDetails.put("serviceName", (String) "Haircut");
 
         if(isBookingValid){
             try{
                 BookingMessageDTO bookingMessageDTO = new BookingMessageDTO(
-                        savedBooking.getSalonId(),
+                        savedBooking.getId(),
+                        bookingDetails.get("salonName"),
+                        bookingDetails.get("salonAddress"),
+                        bookingDetails.get("salonPhone"),
+                        bookingDetails.get("salonEmail"),
                         savedBooking.getTStart(),
                         savedBooking.getTEnd(),
-                        savedBooking.getSlotId(),
-                        savedBooking.getClientId(),
-                        savedBooking.getEmployeeId(),
-                        savedBooking.getServiceId(),
-                       savedBooking.getPayed()
+                        bookingDetails.get("clientName"),
+                        bookingDetails.get("clientSurname"),
+                        bookingDetails.get("clientEmail"),
+                        bookingDetails.get("serviceName"),
+                        savedBooking.getPayed()
                 );
+
+                log.info("BookingMessageDTO created: {}, {}, {}", bookingMessageDTO.getSalonName(), bookingMessageDTO.getTStart(), bookingMessageDTO.getTEnd());
 
                 log.info("Booking {} approved. Publishing BookingMessageDTO to exchange '{}' with key '{}'",
                         savedBooking.getId(), exchangeName, bookingCreatedRoutingKey);
@@ -265,12 +289,21 @@ public class BookingService {
     private boolean validateSalon(Long salonId) {
         log.info("Calling Salon Service to validate salon {}", salonId);
         try {
-            salonWebClient.get()
+            String jsonResponse = salonWebClient.get()
                     .uri("/api/salons/{id}", salonId)
                     .retrieve() // Ottiene la risposta
-                    .toBodilessEntity() // Non ci interessa il body, solo lo status code
+                    .bodyToMono(String.class)
                     .block(); // Blocca finché la chiamata non è completa (ATTENZIONE: non ideale in scenari high-concurrency)
             log.info("Salon {} validation successful", salonId);
+
+            ObjectMapper mapper = new ObjectMapper();
+            Map<String, Object> responseMap = mapper.readValue(jsonResponse, new TypeReference<Map<String, Object>>() {});
+
+            bookingDetails.put("salonName", (String) responseMap.get("name"));
+            bookingDetails.put("salonAddress", (String) responseMap.get("address"));
+            bookingDetails.put("salonPhone", (String) responseMap.get("phone"));
+            bookingDetails.put("salonEmail", (String) responseMap.get("email"));
+
             return true;
         } catch (WebClientResponseException e) {
             log.warn("Salon validation failed for {}: Status Code {}", salonId, e.getStatusCode(), e);
@@ -284,12 +317,19 @@ public class BookingService {
     private boolean validateSlot(Long slotId) {
         log.info("Calling Salon Service to validate slot {}", slotId);
         try {
-            salonWebClient.get()
+            String jsonResponse = salonWebClient.get()
                     .uri("/api/timeslots/{id}", slotId)
                     .retrieve() // Ottiene la risposta
-                    .toBodilessEntity() // Non ci interessa il body, solo lo status code
+                    .bodyToMono(String.class) // Non ci interessa il body, solo lo status code
                     .block(); // Blocca finché la chiamata non è completa (ATTENZIONE: non ideale in scenari high-concurrency)
             log.info("Slot {} validation successful", slotId);
+
+            ObjectMapper mapper = new ObjectMapper();
+            Map<String, Object> responseMap = mapper.readValue(jsonResponse, new TypeReference<Map<String, Object>>() {});
+
+            bookingDetails.put("startTime", (String) responseMap.get("start"));
+            bookingDetails.put("endTime", (String) responseMap.get("end"));
+
             return true;
         } catch (WebClientResponseException e) {
             log.warn("Slot validation failed for {}: Status Code {}", slotId, e.getStatusCode(), e);
@@ -303,12 +343,21 @@ public class BookingService {
     private boolean validateClient(Long clientId) {
         log.info("Calling Client Service to validate client {}", clientId);
         try {
-            clientWebClient.get()
+            String jsonResponce = clientWebClient.get()
                     .uri("/api/clients/getClient/{id}", clientId)
                     .retrieve() // Ottiene la risposta
-                    .toBodilessEntity() // Non ci interessa il body, solo lo status code
+                    .bodyToMono(String.class) // Non ci interessa il body, solo lo status code
                     .block(); // Blocca finché la chiamata non è completa (ATTENZIONE: non ideale in scenari high-concurrency)
             log.info("Client {} validation successful", clientId);
+
+            ObjectMapper mapper = new ObjectMapper();
+            Map<String, Object> responseMap = mapper.readValue(jsonResponce, new TypeReference<Map<String, Object>>() {});
+
+            bookingDetails.put("clientName", (String) responseMap.get("name"));
+            bookingDetails.put("clientSurname", (String) responseMap.get("surname"));
+            bookingDetails.put("clientEmail", (String) responseMap.get("email"));
+
+
             return true;
         } catch (WebClientResponseException e) {
             log.warn("Client validation failed for {}: Status Code {}", clientId, e.getStatusCode(), e);
@@ -326,12 +375,18 @@ public class BookingService {
     private boolean validateService(Long serviceId) {
         log.info("Calling Salon Service to validate service {}", serviceId);
         try {
-            salonWebClient.get()
+            String jsonResponce = salonWebClient.get()
                     .uri("/api/services/{id}", serviceId)
                     .retrieve() // Ottiene la risposta
-                    .toBodilessEntity() // Non ci interessa il body, solo lo status code
+                    .bodyToMono(String.class) // Non ci interessa il body, solo lo status code
                     .block(); // Blocca finché la chiamata non è completa (ATTENZIONE: non ideale in scenari high-concurrency)
             log.info("Service {} validation successful", serviceId);
+
+            ObjectMapper mapper = new ObjectMapper();
+            Map<String, Object> responseMap = mapper.readValue(jsonResponce, new TypeReference<Map<String, Object>>() {});
+
+            bookingDetails.put("serviceName", (String) responseMap.get("name"));
+
             return true;
         } catch (WebClientResponseException e) {
             log.warn("Service validation failed for {}: Status Code {}", serviceId, e.getStatusCode(), e);
