dc92cb083f9637355919c24c9e3333eee12c5704
Matteo Bussolino <matteobussolino7@gmail.com>
2025-07-04 01:17:46 +0200

    Changed the values sent for the notification service

--- backend/message-common/src/main/java/com/taass/BookingMessageDTO.java
+++ backend/message-common/src/main/java/com/taass/BookingMessageDTO.java
@@ -1,35 +1,34 @@
 package com.taass;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.io.Serial;
 import java.io.Serializable;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookingMessageDTO implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
-    private Long salonId;
+    private Long orderId;
+
+    private String SalonName;
+    private  String salonAddress;
+    private String salonPhone;
+    private String salonEmail;
+
     private String tStart;
     private String tEnd;
-    private Long slotId;
-    private Long employeeId;
-    private Long serviceId;
-    private Boolean payed;
 
-    public BookingMessageDTO(Long salonId, String tStart, String tEnd, Long slotId, Long clientId, Long employeeId, Long serviceId, Boolean payed) {
-        this.salonId = salonId;
-        this.tStart = tStart;
-        this.tEnd = tEnd;
-        this.slotId = slotId;
-        this.employeeId = employeeId;
-        this.serviceId = serviceId;
-        this.payed = payed;
-    }
+    private String clientName;
+    private String clientSurname;
+    private  String clientEmail;
+
+    private String serviceName;
+
+    private Boolean payed;
 }
