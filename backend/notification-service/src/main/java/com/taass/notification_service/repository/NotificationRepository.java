1d728bbbf1613dc160d88c3598a0bd42e74863ec
Matteo Bussolino <matteobussolino7@gmail.com>
2025-07-04 01:30:24 +0200

    Removed useless classes

--- backend/notification-service/src/main/java/com/taass/notification_service/repository/NotificationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.taass.notification_service.repository;
-
-import com.taass.notification_service.model.Notification;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
-
-}
