d28b99cfdb8df334322afb3498f7e0df08850a53
Poutchy <94742805+Poutchy@users.noreply.github.com>
2025-07-05 17:18:37 +0200

    small redirection modifications

--- frontend/src/page/salons.jsx
+++ frontend/src/page/salons.jsx
@@ -1,22 +1,8 @@
 import "../css/salons.css"
-import React, {useEffect} from "react";
+import React from "react";
 import {Salons} from "../components";
-import {useLocation, useNavigate} from "react-router-dom";
 
 export function SalonsVisualisation(){
-    const navigate = useNavigate();
-    const location = useLocation();
-    const { salon, service } = location.state || {};
-
-    useEffect(() => {
-        if (salon && service) {
-            navigate("/reservation");
-        }
-    }, [salon, service, navigate]);
-
-    if (salon && service) {
-        return null; // ⏳ Or render a loading spinner or redirect message
-    }
     return (
         <div>
             <Salons/>
