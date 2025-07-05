05554c89810ee9b149b901935e615a8542104a6a
Matteo Bussolino <Matteobussolino7@gmail.com>
2025-07-05 18:09:42 +0200

    Small change in configuration

--- docker/mysql/init-script.sql
+++ docker/mysql/init-script.sql
@@ -2,10 +2,12 @@
 CREATE DATABASE IF NOT EXISTS client_service;
 CREATE DATABASE IF NOT EXISTS booking_service;
 CREATE DATABASE IF NOT EXISTS payment_service;
+CREATE DATABASE IF NOT EXISTS auth_service;
 
 -- Create user and grant privileges (if needed)
 CREATE USER IF NOT EXISTS 'tahabens'@'%' IDENTIFIED BY 'taassbewell';
 GRANT ALL PRIVILEGES ON client_service.* TO 'tahabens'@'%';
 GRANT ALL PRIVILEGES ON booking_service.* TO 'tahabens'@'%';
 GRANT ALL PRIVILEGES ON payment_service.* TO 'tahabens'@'%';
+GRANT ALL PRIVILEGES ON auth_service.* TO 'tahabens'@'%';
 FLUSH PRIVILEGES;
